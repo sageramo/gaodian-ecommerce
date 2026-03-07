a97dbc0461aa1377b9eb832ec013dd361b7ac025
sageramo <sageramo.01@gmail.com>
2026-03-07 15:35:56 +0800

    Delete pages directory

--- pages/CheckoutPage.jsx
+++
@@ -1,5 +0,0 @@
-function CheckoutPage() {
-  return <div style={{ padding: '20px' }}><h1>Checkout Page</h1></div>;
-}
-
-export default CheckoutPage;
--- pages/ProductDetailPage.jsx
+++
@@ -1,5 +0,0 @@
-function ProductDetailPage() {
-  return <div style={{ padding: '20px' }}><h1>Product Detail Page</h1></div>;
-}
-
-export default ProductDetailPage;
